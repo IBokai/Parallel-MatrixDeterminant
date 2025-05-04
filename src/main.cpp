ae383752ac068bfcc9aa80c5b178d74812d7f4eb
Ivan <bokai.box@gmail.com>
2025-05-04 16:10:38 +0300

    feat: improve cli

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,33 @@
 #include "matrix/matrix.h"
 
 int main(int argc, char* argv[]) {
-    std::ifstream file("../matrices/matrix8.txt");
-    Matrix matrix = Matrix(file);
     long double result = 0;
-    if (argc == 1) {
+    if (argc != 3) {
+        std::cout << "Wrong arguments\n";
+        return -1;
+    } else if (std::string(argv[1]) == "parallel") {
+        std::string filename = argv[2];
+        std::string filepath = "../matrices/" + filename;
+        std::ifstream file(filepath);
+        Matrix matrix = Matrix(file);
+        std::cout << "Executing parallel version\n";
+        // measure time here using std::system_clock::now()
+        auto before = std::chrono::system_clock::now();
+        result = CalculateDeterminant(std::move(matrix));
+        auto after = std::chrono::system_clock::now();
+        auto time = std::chrono::duration_cast<std::chrono::microseconds>(after - before);
+        std::cout << "Executing time (microseconds): " << time.count() << '\n';
+    } else if (std::string(argv[1]) == "non-parallel") {
+        std::string filename = argv[2];
+        std::string filepath = "../matrices/" + filename;
+        std::ifstream file(filepath);
+        Matrix matrix = Matrix(file);
         std::cout << "Executing non-parallel version\n";
         auto before = std::chrono::system_clock::now();
         result = CalculateDeterminantNonParallel(std::move(matrix));
         auto after = std::chrono::system_clock::now();
         auto time = std::chrono::duration_cast<std::chrono::microseconds>(after - before);
         std::cout << "Executing time (microseconds): " << time.count() << '\n';
-    } else if (argc == 2) {
-        std::string argument = argv[1];
-        if (argument == "parallel") {
-            std::cout << "Executing parallel version\n";
-            // measure time here using std::system_clock::now()
-            auto before = std::chrono::system_clock::now();
-            result = CalculateDeterminant(std::move(matrix));
-            auto after = std::chrono::system_clock::now();
-            auto time = std::chrono::duration_cast<std::chrono::microseconds>(after - before);
-            std::cout << "Executing time (microseconds): " << time.count() << '\n';
-        } else {
-            std::cout << "Wrong arguments\n";
-            return -1;
-        }
     } else {
         std::cout << "Wrong arguments\n";
         return -1;
